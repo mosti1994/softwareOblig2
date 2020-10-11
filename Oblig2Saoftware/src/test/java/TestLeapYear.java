4abbd8309cf762fbed4ef0beff4c6512a0849442
mosti <mostafam@hiof.no>
2020-10-11 20:20:46 +0200

    forgett gitIgnore

--- Oblig2Saoftware/src/test/java/TestLeapYear.java
+++ Oblig2Saoftware/src/test/java/TestLeapYear.java
@@ -1,5 +1,5 @@
 import no.mostafam.software.Year;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
